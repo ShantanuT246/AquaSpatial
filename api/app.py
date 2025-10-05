23fc2580882a9836605dce56894370fe8887075f
ShantanuT246 <shantanutapole@gmail.com>
2025-10-06 01:47:08 +0530

    vercel error #8

--- api/app.py
+++ api/app.py
@@ -470,3 +470,5 @@ def after_request(response):
 # if __name__ == "__main__":
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == "__main":
+    app.run()
